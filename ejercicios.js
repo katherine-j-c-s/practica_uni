45bb4d4ec1db24090ebad0860e46b78ca2988158
katherine-j-c-s <kathijcs@gmail.com>
2023-08-17 14:26:24 -0300

    add

--- ejercicios.js
+++ ejercicios.js
@@ -7,16 +7,19 @@ export const ejercicios = [
                 id: "a)",
                 tema: "el algoritmo lee tres numeros y muestra la suma de los tres",
                 entrada: 3,
+                ingresa:["ingrese un numero:","ingrese otro numero:","ingrese ultimo numero:"],
                 salida: 1
             },{
                 id: "b)",
                 tema: "sacar el cuadrado de un numero",
                 entrada: 1,
+                ingresa:["ingrese un numero:"],
                 salida: 1
             },{
                 id: "c)",
                 tema: "sacar cuanto obtuvo de ganancias el empleado por hora",
                 entrada: 3,
+                ingresa:["ingrese empleado:","ingrese las horas trabajadas:", "ingrese el valor de la hora:"],
                 salida: 1
             }
         ]
--- src/Components/Results/Ejercicio1.jsx
+++ src/Components/Results/Ejercicio1.jsx
@@ -1,22 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {ejercicios} from '../../../ejercicios'
 
 export default function Ejercicio1() {
+    const [info,setInfo] = useState({
+            primero: [],
+            segundo: 0,
+            tercero: [],
+        }
+    )
+    const handleChange = (event) => {
+        console.log(info);
+        if (event.target.name === "primero") {
+            setInfo({
+                ...info,
+                [event.target.name]: [...info.primero ,event.target.value ]
+            })
+        }else if (event.target.name === "tercero") {
+            setInfo({
+                ...info,
+                [event.target.name]: [...info.tercero ,event.target.value ]
+            })
+        }else{
+            setInfo({
+                ...info,
+                [event.target.name]: event.target.vaue 
+            })
+        }
+        
+        console.log(info);
+    }
   return (
     <div>
         {console.log(ejercicios)}
         <h1>{ejercicios[0].consigna}</h1>
         {ejercicios[0].ejercicios.map((e,i)=>{
+            let numEjer = null
+            if (i === 0) {
+                numEjer = "primero"
+            }else if (i === 1) {
+                numEjer = "segundo"
+            }else if (i === 2) {
+                numEjer = "tercero"
+            }
+            console.log(numEjer);
             return(
-                <div className='my-10'>
+                <div key={i} className='my-10'>
                     <div className='flex'>
                         <p>{e.id}</p>
                         <p>{e.tema}</p>
                     </div>
                     <div>
                         <p>pasos:</p>
+                        {Array(e.entrada).fill().map((_,i)=>{
+                            return(
+                                <div key={i}>
+                                    <p>entrada:</p>
+                                    {console.log()}
+                                    <input 
+                                        type="text" 
+                                        name={numEjer}
+                                        placeholder={e.ingresa[i]} 
+                                        value={typeof info[numEjer] === "object" ? info[numEjer][i] : info[numEjer]}
+                                        onChange={handleChange}
+                                    />
+                                    {++i !== e.entrada ?
+                                        <button>next</button>
+                                    : null}
+                                </div>
+                            )
+                        })}
+                        <button>resolver</button>
                     </div>
-                    {console.log(i)}
                 </div>
             )
         })}
